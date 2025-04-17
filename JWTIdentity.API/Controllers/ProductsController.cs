46ecaf43db91b5e71003b877516b0dd6d108e442
AzmiOzmn <azmiozmen5234@gmail.com>
2025-04-17 21:31:31 +0300

    update

---
+++ JWTIdentity.API/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using JWTIdentity.API.Context;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTIdentity.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProductsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("GetProducts")]
+        public IActionResult GetProducts()
+        {
+            var products = _context.Products.ToList();
+            return Ok(products);
+        }
+    }
+}
